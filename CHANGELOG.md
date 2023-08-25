4bb58c4f41a273b316a6172326b4701edb5563c7
Chien-Hong Chan <woofdogtw@hotmail.com>
2023-08-25 20:43:08 +0800

    Fix SDK middleware.
    - sdk: Fix processing token of the auth middleware.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 0.0.7 - 2023-08-25
+
+### Fixed
+
+- **sdk**: Fix processing token of the auth middleware.
+
 ## 0.0.6 - 2023-08-20
 
 ### Changed
--- sdk/middlewares/doc.go
+++ sdk/middlewares/doc.go
@@ -7,6 +7,8 @@ The authentication middleware by sending the Bearer token to `sylvia-iot-auth`.
 Here is an example to wrap the auth middleware and how to get token information:
 
 	import (
+		"fmt"
+
 		"github.com/gin-gonic/gin"
 		"github.com/woofdogtw/sylvia-iot-go/sdk/middlewares"
 	)
@@ -15,6 +17,9 @@ Here is an example to wrap the auth middleware and how to get token information:
 		r := gin.Default()
 		r.Use(middlewares.AuthMiddleware("http://localhost:1080/auth/api/v1/auth/tokeninfo"))
 		r.GET("/", func(c *gin.Context) {
+			_tokenInfo, _ := c.Get(middlewares.TokenInfoKey)
+			tokenInfo, _ := _tokenInfo.(middlewares.FullTokenInfo)
+			fmt.Printf("%+v\n", tokenInfo)
 			c.AbortWithStatus(204)
 		})
 		r.Run()
